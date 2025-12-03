b9b2aa0f110591bfca5dd1b1f74600a5cdefd4e2
Alvaro Parada <paradalvaro@gmail.com>
2025-12-02 23:43:48 -0400

    axios base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  //baseURL: 'http://localhost:5000/api',
+  baseURL: "https://invoice-server-ecru.vercel.app/api",
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
